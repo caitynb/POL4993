c2b0f908e20e84b3e5cf147a609bff2b6eee049a
Caitlyn (Caity) Barrett <90736239+caitynb@users.noreply.github.com>
2023-02-05 12:05:07 -0600

    Update ess.R

--- ess.R
+++ ess.R
@@ -1,17 +1,48 @@
+############################################################################
+### ESS 2020
+
+# clear environment
 rm(list=ls())
-library(haven)
+
+# load libraries
+library(car)
 library(psych)
+library(ggplot2)
+library(AER)
+library(estimatr)
+library(summarytools)
+library(dotwhisker)
+library(dplyr)
+library(lavaan)
+library(hrbrthemes)
+library(survey)
+library(tidyverse)
+library(interactions)
 library(corrplot)
 library(Hmisc)
-ess8 <- read_dta(file="ess8.dta")
+library(haven)
+library(poliscidata)
+library(lme4)
+library(lmerTest)
+library(devtools)
 
-#federico 0-1 recode
+# import data
+ess8 <- read_dta(file="ESS8e02_2.dta")
+
+# load coded Rdata:
+load("ess8.Rdata")
+
+# federico 0-1 recode
 std01<-function(x){
   min.x<-min(x, na.rm=T)
   max.x<-max(x-min.x, na.rm=T)
   return((x-min.x)/max.x)}
 
+############################################################################
+###### coding
+
 ######demographics
+
 ## income
 ess8$rinc01<-std01(ess8$hinctnta)
 
@@ -41,6 +72,13 @@ ess8$rmin<-as_factor(Recode(ess8$rmin, "1=1; 2=0; 3:5=NA"))
 ## union member
 ess8$union<-ifelse(ess8$mbtru<3, 1, 0)
 
+#### political interest
+ess8$int<-as.numeric(as_factor(ess8$polintr))
+ess8$int<-Recode(ess8$int, "1=4; 2=3; 3=2; 4=1; 5:7=NA")
+ess8$rint<-std01(ess8$int)
+ess8$intf<-as.factor(ess8$int)
+
+ess8$intf<-factor(ess8$intf, labels = c("Not at All", "Hardly", "Quite", "Very"))
 
 #####authoritarianism/conservation
 #### value recodes.
@@ -77,6 +115,7 @@ ess8$rrach1<-(ess8$ipsuces-1)
 ess8$rrach2<-(ess8$ipshabt-1)
 ess8$rrpow1<-(ess8$imprich-1)
 ess8$rrpow2<-(ess8$iprspot-1)
+
 ## security values
 ess8$rsec<-std01(rowMeans(with(ess8, cbind(rsec1, rsec2))))
 
@@ -86,18 +125,20 @@ ess8$rcon<-std01(rowMeans(with(ess8, cbind(rcon1, rcon2))))
 ## tradition values
 ess8$rtrad<-std01(rowMeans(with(ess8, cbind(rtrad1, rtrad2))))
 
-## conservation values
+## conservation values (high = greater endorsement of CV)
 psych::alpha(with(ess8, cbind(rsec1, rsec2, rcon1, rcon2, rtrad1, rtrad2)))
 ess8$rcv<-std01(rowMeans(with(ess8, cbind(rsec1, rsec2, rcon1, rcon2, rtrad1, rtrad2))))
 
 
-####################benefits received 0-1
+#################### benefits received 0-1
+# 1 --> did not receive public assistance; 0 = did receive assistance.
+
 table(ess8$hincsrca)
+
+ess8$pubasst1<-ifelse(ess8$hincsrca==6|ess8$hincsrca==5,0,1)
 table(ess8$pubasst1)
-ess8$pubasst1<-ifelse(ess8$hincsrca==6|ess8$hincsrca==5,1,0)
 
-
-#######################economic preferences
+####################### economic preferences
 ess8$rec1<-ifelse(ess8$gincdif<6, (ess8$gincdif-1)/4, NA)
 table(ess8$gincdif)
 table(ess8$rec1)
@@ -110,10 +151,144 @@ table(ess8$rec3)
 ess8$rec4<-ifelse(ess8$gvcldcr<11, (10-ess8$gvcldcr)/10, NA)
 table(ess8$gvcldcr)
 table(ess8$rec4)
-##economic style
+
+## economic preference scale (high = more right-wing/market oriented)
 psych::alpha(with(ess8, cbind(rec1, rec2, rec3, rec4)))
 ess8$recsc1<-rowMeans(with(ess8, cbind(rec1, rec2, rec3, rec4)))
 
 ##check correlations for eco pref items
 ecopref<-cbind.data.frame(ess8$rec1, ess8$rec2,ess8$rec3,ess8$rec4)
 ecocorr<-rcorr(as.matrix(ecopref), type="pearson")
+
+#### recode nation variable
+ess8$nat1<-as_factor(ess8$cntry)
+ess8$nat1<-factor(ess8$cntry,
+                  labels=c("Austria",
+                           "Belgium",
+                           "Switzerland",
+                           "Czechia",
+                           "Germany",
+                           "Estonia",
+                           "Spain",
+                           "Finland",
+                           "France",
+                           "United Kingdom",
+                           "Hungary",
+                           "Ireland",
+                           "Israel",
+                           "Iceland",
+                           "Italy",
+                           "Lithuania",
+                           "Netherlands",
+                           "Norway",
+                           "Poland",
+                           "Portugal",
+                           "Russia",
+                           "Sweden",
+                           "Slovenia"))
+ess8$nat2<-as.numeric(ess8$nat1)
+
+## save data in R format
+save(ess8, file="ess8.Rdata")
+
+################################################################################
+######## weighting setup
+
+### WEIGHTS AVAILABLE: pspwght, pweight, dweight, ***anweight
+### NOTE: anweight = pspwght*pweight
+
+### rescale weights for use in lme4
+### rescaled weights: pweights_a and pweights_b
+### use pweights_a for point estimates (Carle 2009)
+library(datawizard)
+ess8<-rescale_weights(ess8, "nat1", "anweight", nest = FALSE)
+
+# https://easystats.github.io/datawizard/reference/rescale_weights.html
+
+################################################################################
+################################################################################
+######## regressions -- mixed models
+
+###### main model
+wm1<-lmer(recsc1~1+rinc01+rage+male+edr3+rmin+union+intf+rcv*pubasst1+
+            (1+rcv|nat1), weights=pweights_a, data = ess8, REML = F)
+summary(wm1)
+
+### conditional effects
+sim_margins(wm1, pred = rcv, modx = pubasst1)
+
+#### plot, without auth x information interaction
+f4 <- interact_plot(wm1, pred = "rcv", 
+                    modx = "pubasst1", 
+                    interval = TRUE, 
+                    legend.main = "Receive Assistance?",
+                    modx.values = NULL, 
+                    modx.labels=c("Yes", "No"), 
+                    colors="CUD Bright") + 
+  scale_y_continuous(breaks=seq(0,1,0.2), limits=c(0,1)) + 
+  scale_x_continuous(breaks=seq(0,1,0.2), limits=c(0,1)) + 
+  theme_bw() + 
+  theme(aspect.ratio=1, 
+        plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Social Spending, Conservation Values, and Assistance Receipt\n(Without Information Interaction)",
+       x = "Conservation Values (0-1)",
+       y = "Opposition to Social Spending (0-1)")
+ggsave(file="f4.png", f4, width = 10, height = 8)
+
+###### main model with interest interaction
+wm2<-lmer(recsc1~1+rinc01+rage+male+edr3+rmin+union+intf+rcv*pubasst1+rcv*intf+
+            (1+rcv|nat1), weights=pweights_a, data = ess8, REML = F)
+summary(wm2)
+
+### conditional effects
+### note that with factor variables, sim_margins will sometimes report the
+### conditional effects in alphabetical order of the labels rather than the
+### underlying values -- be careful with this!
+sim_margins(wm2, pred = rcv, modx = pubasst1)
+sim_margins(wm2, pred = rcv, modx = intf)
+
+#### plot, with auth x information interaction
+f5 <- interact_plot(wm2, pred = "rcv", 
+                    modx = "pubasst1", 
+                    interval = TRUE, 
+                    legend.main = "Receive Assistance?",
+                    modx.values = NULL, 
+                    modx.labels=c("Yes", "No"), 
+                    colors="CUD Bright") + 
+  scale_y_continuous(breaks=seq(0,1,0.2), limits=c(0,1)) + 
+  scale_x_continuous(breaks=seq(0,1,0.2), limits=c(0,1)) + 
+  theme_bw() + 
+  theme(aspect.ratio=1, 
+        plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Social Spending, Conservation Values, and Assistance Receipt\n(Without Information Interaction)",
+       x = "Conservation Values (0-1)",
+       y = "Opposition to Social Spending (0-1)")
+ggsave(file="f5.png", f5, width = 10, height = 8)
+
+#### plot of auth x information interaction
+f6 <- interact_plot(wm2, pred = "rcv", 
+                    modx = "intf", 
+                    interval = TRUE, 
+                    legend.main = "Interested in Politics?",
+                    modx.values = NULL, 
+                    #modx.labels=c("Yes", "No"), 
+                    colors="CUD Bright") + 
+  scale_y_continuous(breaks=seq(0,1,0.2), limits=c(0,1)) + 
+  scale_x_continuous(breaks=seq(0,1,0.2), limits=c(0,1)) + 
+  theme_bw() + 
+  theme(aspect.ratio=1, 
+        plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Social Spending, Conservation Values, and Interest",
+       x = "Conservation Values (0-1)",
+       y = "Opposition to Social Spending (0-1)")
+ggsave(file="f6.png", f6, width = 10, height = 8)
+
+#####make regression table 
+
+devtools::install_github("jacob-long/jtools",force=TRUE)
+## getting an error
+esst2<-huxreg(wm1, wm2, 
+              statistics = c("N" = "nobs"),
+              number_format = 2)
+quick_docx(esst2, file='esst2.docx')
+
